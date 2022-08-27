eb1a946acbb10516ea54beaed5f62a03eb67c948
Fiddy <85234199+Felixx2112@users.noreply.github.com>
2022-08-27 18:32:12 +0700

    submit form

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ChatForm from "../components/ChatForm";
+import Sidebar from "../components/Sidebar";
 
 function Chat() {
-  return <div>chat</div>;
+  return (
+    <Container>
+      <Row>
+        <Col md={4}>
+          <Sidebar />
+        </Col>
+        <Col md={8}>
+          <ChatForm />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Chat;
